5ee8b735c4c04b68237f878ec6fcc62b0835ca25
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-04-26 23:29:49 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -283,7 +283,7 @@ async def start(client, message):
                   
                     ]
                     ]
-   k = await client.send_cached_media(
+    k = await client.send_cached_media(
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
