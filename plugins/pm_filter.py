c424f3cc66dc16477259202a575e75bdfeaf4326
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-04-26 15:04:25 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1187,10 +1187,7 @@ async def auto_filter(client, msg: pyrogram.types.Message, spoll=False):
     )
   
     
-    await message.reply_chat_action("Typing")
-    m=await message.reply_sticker("CAACAgUAAx0CQTCW0gABB5EUYkx6-OZS7qCQC6kNGMagdQOqozoAAgQAA8EkMTGJ5R1uC7PIECME") 
-    await asyncio.sleep(2)
-    await m.delete()
+    
     
 
     if offset != "":
@@ -1355,20 +1352,19 @@ async def advantage_spell_chok(msg):
     await asyncio.sleep(2)
     await m.delete()
 
-    await asyncio.sleep(60)
-    await k.delete()  
+      
     
 
-    fmsg = await msg.reply_photo(
+    k = await msg.reply_photo(
         photo= "https://telegra.ph/file/8a8ba3e824e1d2482253f.jpg",
         caption=f"<b>📍നിങ്ങളുടെ സിനിമ ഇതിലുണ്ടോന്ന് പരിശോധിക്കുക</b>",
         parse_mode="html",
         reply_markup=InlineKeyboardMarkup(btn))
-                  
+
+    await asyncio.sleep(60)
+    await k.delete()                
     return k
-        
-    
-    
+                
 async def manual_filters(client, message, text=False):
     group_id = message.chat.id
     name = text or message.text
