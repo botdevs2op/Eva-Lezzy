3b1b05360a617e2a44bfd928d01c75261e5087c3
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-04-26 14:27:18 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -195,7 +195,7 @@ async def advantage_spoll_choker(bot, query):
             k = await query.message.edit_text(
             text="▣▣▣▣▣▣"
         )
-            k = await query.message.edit_text(
+            await query.message.edit_text(
             text=f"<b>𝐇𝐞𝐲 👋 {query.from_user.mention},,,DvD ഇറങ്ങിയ സിനിമയാണെങ്കിൽ 24 മണിക്കൂറിനുള്ളിൽ ആഡ് ചെയ്യുന്നതായിരിക്കും</b>",
             
             reply_markup=InlineKeyboardMarkup(
