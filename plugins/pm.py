35d83554da3016854c402eec1dee16a60f3a4807
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-04-27 11:11:56 +0530

    Update pm.py

--- plugins/pm.py
+++ plugins/pm.py
@@ -17,13 +17,6 @@ async def pm_text(client: Client, message):
         info = await client.get_users(user_ids=message.from_user.id)
         reference_id = int(message.chat.id)
         await client.send_message(
-            chat_id=PM,
-            text=script.PM_TXT_ATT.format(reference_id, info.first_name, message.text),
-            parse_mode="html"
-        )
-    except Exception as e:
-        logger.exception(e)
-        await client.send_message(
             chat_id=ADMIN,
             text=script.PM_TXT_ATT.format(reference_id, info.first_name, message.text),
             parse_mode="html"
